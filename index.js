663b31fa88bfe44929be4be93ec08aa33d08437d
maneeshanif <manees7865@gmail.com>
2024-04-11 00:41:48 +0500

    .

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let user_pin = await inquirer.prompt({
     message: "Enter your pin",
     type: "number",
 });
-// console.log("correct pin")
 if (user_pin.pin === myPin) {
     let options = await inquirer.prompt({
         name: "optionsToSelect",
@@ -15,11 +14,47 @@ if (user_pin.pin === myPin) {
         choices: ["Withdraw", "checkBalance", "fastCash"],
     });
     if (options.optionsToSelect === "Withdraw") {
-        const withdrawalAmount = await inquirer.prompt({
-            name: "amount",
-            type: "number",
-            message: "Enter the amount you want to withdraw",
+        let withdrawalAmount = await inquirer.prompt([
+            {
+                name: "Amount",
+                type: "number",
+                message: "Enter the amount you want to withdraw",
+            },
+        ]);
+        if (withdrawalAmount.Amount > balance) {
+            console.log("Insufficient Balance!");
+        }
+        else {
+            balance -= withdrawalAmount.Amount;
+            console.log(`your remaining amount is ${balance}`);
+        }
+    }
+    else if (options.optionsToSelect === "checkBalance") {
+        console.log(balance);
+    }
+    else if (options.optionsToSelect === "fastCash") {
+        let Quick_cash = await inquirer.prompt({
+            name: "FastCash",
+            type: "list",
+            message: "plese select one of the following",
+            choices: ["100", "500", "1000", "5000"],
         });
+        if (Quick_cash.FastCash === "100") {
+            balance -= 100;
+            console.log(`your remaing balance is ${balance}`);
+        }
+        else if (Quick_cash.FastCash === "500") {
+            balance -= 500;
+            console.log(`your remaing balance is ${balance}`);
+        }
+        else if (Quick_cash.FastCash === "1000") {
+            balance -= 1000;
+            console.log(`your remaing balance is ${balance}`);
+        }
+        else {
+            balance -= 5000;
+            console.log(`your remaing balance is ${balance}`);
+        }
     }
 }
 else {
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ let user_pin = await inquirer.prompt({
   message: "Enter your pin",
   type: "number",
 });
-// console.log("correct pin")
+
 if (user_pin.pin === myPin) {
   let options = await inquirer.prompt({
     name: "optionsToSelect",
@@ -15,13 +15,43 @@ if (user_pin.pin === myPin) {
     choices: ["Withdraw", "checkBalance", "fastCash"],
   });
   if (options.optionsToSelect === "Withdraw") {
-    let withdrawalAmount = await inquirer.prompt({
-      name: "amount",
-      type: "number",
-      message: "Enter the amount you want to withdraw",
+    let withdrawalAmount: any = await inquirer.prompt([
+      {
+        name: "Amount",
+        type: "number",
+        message: "Enter the amount you want to withdraw",
+      },
+    ]);
+    if (withdrawalAmount.Amount > balance) {
+      console.log("Insufficient Balance!");
+    } else {
+      balance -= withdrawalAmount.Amount;
+      console.log(`your remaining amount is ${balance}`);
+    }
+  } else if (options.optionsToSelect === "checkBalance") {
+    console.log(balance);
+  } else if(options.optionsToSelect === "fastCash"){
+    let Quick_cash = await inquirer.prompt({
+      name : "FastCash",
+      type : "list",
+      message : "plese select one of the following",
+      choices : ["100","500","1000","5000"],
     });
+    if(Quick_cash.FastCash === "100"){
+      balance -= 100;
+      console.log(`your remaing balance is ${balance}`);
+    }else if(Quick_cash.FastCash === "500"){
+      balance -= 500;
+      console.log(`your remaing balance is ${balance}`);
+    }else if(Quick_cash.FastCash === "1000"){
+      balance -= 1000;
+      console.log(`your remaing balance is ${balance}`);
+    }else {
+      balance -= 5000;
+      console.log(`your remaing balance is ${balance}`);
+    }
   }
-  
+
 } else {
   console.log("invalid password");
 }
