a2dd27320ff57bf0d2d0f7160054039d2d3f299d
maneeshanif <manees7865@gmail.com>
2024-04-10 19:00:31 +0500

    .

--- index.ts
+++ index.ts
@@ -1,28 +1,27 @@
 import inquirer from "inquirer";
 let balance = 10000;
 let myPin = 1308;
-let userAnswer = await inquirer.prompt(
-    [
-        {
-            name : "pin",
-            message : "Enter your pin",
-            type : " number",
-        }
-    ]);
-    console.log("correct pin")
-    if (userAnswer.pin === myPin){
-        let options = await inquirer.prompt([
-            
-            {
-                name : "optionsToSelect",
-                type : "list",
-                message : "Select operation you want to perform",
-                choices : ["Withdraw","checkBalance","fastCash"],
-            }
-        ])
-    //   if (options.optionsToSelect === "Withdraw"){
-        //    console.log("lll")
-    //   }
-     }
-
- 
+let user_pin = await inquirer.prompt({
+  name: "pin",
+  message: "Enter your pin",
+  type: "number",
+});
+// console.log("correct pin")
+if (user_pin.pin === myPin) {
+  let options = await inquirer.prompt({
+    name: "optionsToSelect",
+    type: "list",
+    message: "Select operation you want to perform",
+    choices: ["Withdraw", "checkBalance", "fastCash"],
+  });
+  if (options.optionsToSelect === "Withdraw") {
+    let withdrawalAmount = await inquirer.prompt({
+      name: "amount",
+      type: "number",
+      message: "Enter the amount you want to withdraw",
+    });
+  }
+  
+} else {
+  console.log("invalid password");
+}
