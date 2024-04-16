f2955a35fbf297ff125667145011b1a8fe033639
maneeshanif <manees7865@gmail.com>
2024-04-16 17:12:14 +0500

    add some styling

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.cyanBright.bold.bgWhiteBright.underline('\t\t\t\t\t Auto Teller Machine'))
+console.log(chalk.yellowBright('\t\t\t\t\t\t\t\t\t\t\t\t\tDeveloped By:'))
+console.log(chalk.greenBright.underline("\t\t\t\t\t\t\t\t\t\t\t\t\t\tAnees Hanif"))
+
+
 let balance = 10000;
 let myPin = 1308;
 let user_pin = await inquirer.prompt({
   name: "pin",
-  message: "Enter your pin",
+  message: chalk.blueBright("Enter your pin"),
   type: "number",
 });
 
@@ -11,7 +19,7 @@ if (user_pin.pin === myPin) {
   let options = await inquirer.prompt({
     name: "optionsToSelect",
     type: "list",
-    message: "Select operation you want to perform",
+    message: chalk.red("Select operation you want to perform"),
     choices: ["Withdraw", "checkBalance", "fastCash"],
   });
   if (options.optionsToSelect === "Withdraw") {
@@ -19,39 +27,39 @@ if (user_pin.pin === myPin) {
       {
         name: "Amount",
         type: "number",
-        message: "Enter the amount you want to withdraw",
+        message: chalk.yellowBright("Enter the amount you want to withdraw"),
       },
     ]);
     if (withdrawalAmount.Amount > balance) {
-      console.log("Insufficient Balance!");
+      console.log(chalk.redBright.overline("Insufficient Balance!"));
     } else {
       balance -= withdrawalAmount.Amount;
-      console.log(`your remaining amount is ${balance}`);
+      console.log(chalk.cyanBright.bold(`Your remaining amount is ${balance}`));
     }
   } else if (options.optionsToSelect === "checkBalance") {
-    console.log(balance);
+    console.log(chalk.magenta(balance));
   } else if(options.optionsToSelect === "fastCash"){
     let Quick_cash = await inquirer.prompt({
       name : "FastCash",
       type : "list",
-      message : "plese select one of the following",
+      message : chalk.red.bgGreenBright("plese select one of the following"),
       choices : ["100","500","1000","5000"],
     });
     if(Quick_cash.FastCash === "100"){
       balance -= 100;
-      console.log(`your remaing balance is ${balance}`);
+      console.log(chalk.greenBright.italic.bold`your remaing balance is ${balance}`);
     }else if(Quick_cash.FastCash === "500"){
       balance -= 500;
-      console.log(`your remaing balance is ${balance}`);
+      console.log(chalk.greenBright.italic.bgYellowBright.bold`your remaing balance is ${balance}`);
     }else if(Quick_cash.FastCash === "1000"){
       balance -= 1000;
-      console.log(`your remaing balance is ${balance}`);
+      console.log(chalk.greenBright.italic.bgYellowBright.bold`your remaing balance is ${balance}`);
     }else {
-      balance -= 5000;
-      console.log(`your remaing balance is ${balance}`);
+      balance -= 5000
+      console.log(chalk.greenBright.italic.bgYellowBright.bold`your remaing balance is ${balance}`);
     }
   }
 
 } else {
-  console.log("invalid password");
+  console.log(chalk.underline.red.bold("Invalid password !!!"));
 }
